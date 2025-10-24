335e5036be6c062795387db6349fa897d496cbed
Ron Ferentini <ron.ferentini@gmail.com>
2025-10-23 21:43:28 -0400

    change token functions for customer and add token for mechanic

--- app/utils/utils.py
+++ app/utils/utils.py
@@ -5,7 +5,7 @@ from datetime import datetime, timedelta, timezone
     
 SECRET_KEY = "a super secret key"
 
-def encode_token(customer_id):
+def encode_customer_token(customer_id):
     payload = {
         'exp': datetime.now(timezone.utc) + timedelta(days=0, hours=1),
         'iat': datetime.now(timezone.utc),
@@ -16,7 +16,41 @@ def encode_token(customer_id):
     
     return token
 
-def token_required(f):
+def customer_token_required(f):
+    @wraps(f)
+    def decorated(*args, **kwargs):
+        token = None
+        if "Authorization" in request.headers:
+            token = request.headers["Authorization"].split(" ")[1]
+
+            if not token:
+                return jsonify({"message": "Token is missing!"}), 401
+            
+            try:
+                data = jwt.decode(token, SECRET_KEY, algorithms=["HS256"])
+                current_user = data["sub"]
+            except jwt.ExpiredSignatureError:
+                return jsonify({"message": "Token has expired!"}), 401
+            except jwt.InvalidTokenError:
+                return jsonify({"message": "Invalid token!"}), 401
+            
+            return f(current_user, *args, **kwargs)
+        else:
+            return jsonify({"message": "You must be logged in!"}), 401
+    return decorated
+
+def encode_mechanic_token(mechanic_id):
+    payload = {
+        'exp': datetime.now(timezone.utc) + timedelta(days=0, hours=1),
+        'iat': datetime.now(timezone.utc),
+        'sub': str(mechanic_id)
+    }
+    
+    token = jwt.encode(payload, SECRET_KEY, algorithm='HS256')
+    
+    return token
+
+def mechanic_token_required(f):
     @wraps(f)
     def decorated(*args, **kwargs):
         token = None
